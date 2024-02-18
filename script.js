9ccaa39aa6cab1d45dfc43f351f495dabdbc2c68
Dev-Gitanjali <jgitanjali592@gmail.com>
2024-02-18 13:50:28 +0530

    this styling for image container& style

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const container = document.querySelector('.container')
 const unsplashURL = 'https://source.unsplash.com/random/'
 const rows = 10
 
-for(let i= 0; i< rows*3; i++){
+for(let i= 0; i< rows * 3; i++){
  const img= document.createElement('img')
  img.src=`${unsplashURL} ${getRandomSize()}`
  container.appendChild(img)
@@ -11,7 +11,7 @@ for(let i= 0; i< rows*3; i++){
 }
 
 function getRandomSize(){
-return `${getRandomNr} x ${getRandomNr()}`
+return `${getRandomNr()} x ${getRandomNr()}`
 }
 
 function getRandomNr(){
